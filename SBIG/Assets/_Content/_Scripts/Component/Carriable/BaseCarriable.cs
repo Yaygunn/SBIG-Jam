4168949bccd5ac3d49060baab7a0f8fc79369a32
Yaygun <yusufaygunnn@hotmail.com>
2024-07-01 23:00:53 +0300

    Carriable add bool and text
    IsPickable bool and
    get UI text

--- SBIG/Assets/_Content/_Scripts/Component/Carriable/BaseCarriable.cs
+++ SBIG/Assets/_Content/_Scripts/Component/Carriable/BaseCarriable.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 namespace Components.Carriables
 {
-    public class BaseCarriable : MonoBehaviour
+    public abstract class BaseCarriable : MonoBehaviour
     {
-        public virtual void PickUp(Transform camera) { }
+        [field: SerializeField] public bool IsPickable { get; protected set; } = true;
+        public virtual string GetUiText() { return "____"; }
+        public abstract BaseCarriable PickUp(Transform camera);
         public virtual void Drop() { }
         public virtual void CarryUpdate() { }
     }
