134f64d80bbe5da9eca62dae419be79cbb02bed5
Morne Gouws <webejame@gmail.com>
2024-07-05 14:29:58 +0200

    Blank Bullet Interfaces

---
+++ SBIG/Assets/_Content/_Scripts/Component/BulletHit/IBulletHit.cs
@@ -0,0 +1,19 @@
+namespace Components.BulletHit
+{
+    public interface IWaterHit
+    {
+        public void OnWaterHit();
+    }
+    public interface IBasketBallHit
+    {
+        public void OnBasketBallHit();
+    }
+    public interface ISlapHit
+    {
+        public void OnSlapHit();
+    }
+    public interface IGolemHit
+    {
+        public void GolemHit();
+    }
+}
