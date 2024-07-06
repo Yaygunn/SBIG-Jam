ebbcf4b1b1a43908a58ad12e246fc5b5f9efc727
Morne Gouws <webejame@gmail.com>
2024-07-06 11:19:43 +0200

    Added new models. Added per-golem spawning mechanic

--- SBIG/Assets/_Content/_Scripts/Manager/Temporary_QuitGame.cs
+++ SBIG/Assets/_Content/_Scripts/Manager/Temporary_QuitGame.cs
@@ -1,17 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using Controller.Enemy;
 using Controller.Player;
 using Managers.Global;
-using Scriptables.Enemy;
 using UnityEngine;
 
 public class Temporary_QuitGame : MonoBehaviour
 {
-    [SerializeField] private GameObject _golemPrefab;
-    [SerializeField] private Transform _spawnPoint;
-    [SerializeField] private List<EnemyData> _enemyDataList;
-    
     private PlayerController playerController;
     private Temporary_PreviewUI _previewUI;
 
@@ -24,8 +16,6 @@ public class Temporary_QuitGame : MonoBehaviour
     void Update()
     {
         QuitGame();
-
-        SpawnGolem();
 
         ChangeGameState();
     }
@@ -41,22 +31,6 @@ public class Temporary_QuitGame : MonoBehaviour
             else
             {
                 playerController.ChangeState(playerController.StateCombat);
-            }
-        }
-    }
-
-    private void SpawnGolem()
-    {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            GameObject enemyGO = Instantiate(_golemPrefab, _spawnPoint.position, Quaternion.identity);
-            EnemyController enemy = enemyGO.GetComponent<EnemyController>();
-
-            if (enemy)
-            {
-                int randomIndex = Random.Range(0, _enemyDataList.Count);
-                
-                enemy.Initialize( _enemyDataList[randomIndex] );
             }
         }
     }
