eccc1656af110357061f8c634733957d63650069
Morne Gouws <webejame@gmail.com>
2024-07-06 14:04:53 +0200

    Connected sensitivity in main menu to InputController

--- SBIG/Assets/_Content/_Scripts/Manager/LevelStart/LevelStartManager.cs
+++ SBIG/Assets/_Content/_Scripts/Manager/LevelStart/LevelStartManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Utilities.Singleton;
 using YInput;
 
@@ -12,19 +13,37 @@ namespace Managers.LevelStart
         private void Start()
         {
             InputOperation();
+            UpdateMouseSensitivity();
         }
 
-    private void InputOperation()
-    {
-            if(_startInput == EStartInput.gameplay)
-            {
-                InputHandler.Instance.EnableGameplayMod();
-            }
-            else
-            {
-                InputHandler.Instance.EnableUIMod();
-            }
-    }
+        private void InputOperation()
+        {
+                if(_startInput == EStartInput.gameplay)
+                {
+                    InputHandler.Instance.EnableGameplayMod();
+                }
+                else
+                {
+                    InputHandler.Instance.EnableUIMod();
+                }
+        }
 
+        private void UpdateMouseSensitivity()
+        {
+            float sensitivity = GetMouseSensitivity();
+            
+            SetMouseSensitivity(sensitivity);
+        }
+
+        public void SetMouseSensitivity(float sensitivity)
+        {
+            InputHandler.Instance.SetMouseSensitivity(sensitivity);
+            PlayerPrefs.SetFloat("MouseSensitivity", sensitivity);
+        }
+
+        public float GetMouseSensitivity()
+        {
+            return PlayerPrefs.GetFloat("MouseSensitivity", 1.0f);
+        }
     }
 }
