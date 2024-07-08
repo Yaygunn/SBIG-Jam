33e6b5158d3e73004f26182a97fbc01f6c5e744f
Yaygun <yusufaygunnn@hotmail.com>
2024-07-08 16:46:38 +0300

    cauldron anim change

--- SBIG/Assets/_Content/_Scripts/Component/Cauldron/Original/Cauldron.cs
+++ SBIG/Assets/_Content/_Scripts/Component/Cauldron/Original/Cauldron.cs
@@ -16,9 +16,10 @@ namespace Components.Cauldrons.Original
     {
         List<ECookableType> _cookablesInCauldron = new List<ECookableType>();
         private Animator _animator;
-        readonly private int necesserySize = 3;
+        readonly private int necesserySize = 1;
         private string _isCookingBool = "isCooking";
 
+        [SerializeField] private bool _isCooking;
         private void OnEnable()
         {
             _animator = GetComponentInChildren<Animator>();
@@ -32,29 +33,34 @@ namespace Components.Cauldrons.Original
             if(_cookablesInCauldron.Count>= 3)
             {
                 _cookablesInCauldron.Clear();
-                StartCoroutine(Cooking());
+                Cook2();
+            }
+        }
+        private void Update()
+        {
+            if(_isCooking)
+            {
+                _isCooking = false;
+                Cook2();
             }
         }
 
-        public override void Cook()
+        public void Cook2()
         {
-            if(_cookablesInCauldron.Count > necesserySize)
-            {
-                _cookablesInCauldron.Clear();
-                StartCoroutine(Cooking());
-            }
-            else
-            {
-            }
+
+            _cookablesInCauldron.Clear();
+            UIManager.Instance.HideCraftUI();
+            EventHub.CauldronStartCook();
+            _animator.SetTrigger("cook");
+            Invoke("EndCook", 4);
+
         }
 
         private IEnumerator Cooking()
         {
             float upDuringCooking = 0.5f;
             transform.position += new Vector3(0, upDuringCooking, 0);
-            UIManager.Instance.HideCraftUI();
-            EventHub.CauldronStartCook();
-            _animator.SetBool(_isCookingBool, true);
+            
  
             float cookTime = 3;
             yield return new WaitForSeconds(cookTime);
@@ -64,18 +70,22 @@ namespace Components.Cauldrons.Original
             float cookEndTime = 1.5f;
             yield return new WaitForSeconds(cookEndTime);
 
-            EventHub.CauldronEndCook();
-            EventHub.CookFail();
-
-            UIManager.Instance.ShowCraftUI();
-            transform.position += new Vector3(0, -upDuringCooking, 0);
-            Invoke("EndTurn", 2);
+            
         }
 
         private void EndTurn()
         {
             if(Craft1.Instance != null)
                 Craft1.Instance._continue = false;
+        }
+
+        private void EndCook()
+        {
+            EventHub.CauldronEndCook();
+            EventHub.CookFail();
+
+            UIManager.Instance.ShowCraftUI();
+            Invoke("EndTurn", 2);
         }
     }
 }
